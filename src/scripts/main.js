78d0024200ba45c6b2cbfb3002f9929efcdf9533
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-06-27 18:02:57 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#navMenu') {
-    document.body.classList.add('body--with-menu');
-  } else {
-    document.body.classList.remove('body--with-menu');
-  }
-});
